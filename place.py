d4ae5bb27cac467791e1ddfa49f702301d2cc289
pipopa9886 <46100613+pipopa9886@users.noreply.github.com>
2019-01-19 11:27:42 +0900

    保存可能.ポイントまだ

--- place.py
+++ place.py
@@ -1,7 +1,32 @@
+import json
 import random
 import time
 
 #足し算
+def save():
+    print("もし必要なら結果を記録できます")
+    print("保存しますか")
+    print("はい:0,いいえ:1")
+    save=0
+    save=int(input())
+    if save==0:
+        with open("date.json","r") as file:
+            menu=json.load(file)
+        print("名前を教えてください")
+        name=input()
+        point="100"
+        c=name not in menu
+        if c==True:
+            you={"name":name,"point":point}
+            menu.append(you)
+            with open("date.json","w") as f:
+                json.dump(menu,f)
+        else:
+            print("あるよ")
+    else:
+        print("中止")
+
+
 def addition(keta,kuchi,kankaku):
     list=[]
     sum=0
@@ -13,7 +38,14 @@ def addition(keta,kuchi,kankaku):
     for i in list:
         print("\r{0:d}".format(i),end="")
         time.sleep(kankaku)
-    time.sleep(1.2)
+
+    print("\n答えを入力してください")
+    answer=int(input())
+    if answer==sum:
+        print("正解!")
+    else:
+        print("不正解...")
+    time.sleep(0.5)
     print("\n答え:",sum)
 
 def much():
@@ -28,6 +60,8 @@ def much():
     global kankaku
     kankaku=float(input())
 
+def rurn():
+    save()
 def run():
     much()
     print("始めます。よろしいですか")
@@ -38,3 +72,4 @@ def run():
     else:
         pass
 
+rurn()
